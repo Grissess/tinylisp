c43d6631cbed08866f3b4d672068cb8823f64dd5
Grissess <grissess@nexusg.org>
2022-02-02 17:52:05 -0500

    WASM: Be explicit about exporting the function table

--- minilibc/arch/wasm.mk
+++ minilibc/arch/wasm.mk
@@ -1,4 +1,4 @@
-override LDFLAGS += -Wl,--no-entry -Wl,--allow-undefined -Wl,--export-all -Wl,--no-gc-sections
+override LDFLAGS += -Wl,--no-entry -Wl,--allow-undefined -Wl,--export-all -Wl,--no-gc-sections -Wl,--export-table
 override CFLAGS += --target=wasm32 -DPTR_LSB_AVAILABLE=0
 CC = clang  # allow override
 INTERPRETER = tl.wasm
